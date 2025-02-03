9b5cea5ca6f1695f53a8354c61430b3977395f39
Glafira-Kharitonova <justfirka@gmail.com>
2025-02-03 20:13:34 +0300

    update task.cpp я просто надеюсь, что это работает

--- client/coding/task.cpp
+++ client/coding/task.cpp
@@ -1,10 +1,26 @@
 #include <string>
 
+const int SIZE_OF_ALPH = 33;
+const char FIRST_EL = 'А';
 
 void encoding(char *lis, const std::string& code){
+    int sdvig_of_encode = std::stoi(code);
+    for(int i = 0; lis[i] != '0'; i++) {
+        char now_char = lis[i];
+        if (now_char >= FIRST_EL && now_char <= 'я'){
+            lis[i] = FIRST_EL + (now_char - FIRST_EL + sdvig_of_encode) % SIZE_OF_ALPH;    
+        }
+    }
     return;
 }
 
 void decoding(char *lis, const std::string& code){
+    int sdvig_of_decode = std::stoi(code);
+    for(int i = 0; lis[i] != '0'; i++) {
+        char now_char = lis[i];
+        if (now_char >= FIRST_EL && now_char <= 'я') {
+            lis[i] = FIRST_EL + (now_char - FIRST_EL - sdvig_of_decode + SIZE_OF_ALPH) % SIZE_OF_ALPH;        
+        }
+    }
     return;
 }
